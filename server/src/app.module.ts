477acf5d32390af16d3984805d2850c84984de1b
Aleksandr Pankov <nayteruz@gmail.com>
2022-11-28 17:36:42 +0700

    Init

---
+++ server/src/app.module.ts
@@ -0,0 +1,7 @@
+import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+
+@Module({
+    imports: [TrackModule]
+})
+export class AppModule {}
---
+++ server/src/track/track.controller.ts
@@ -0,0 +1,22 @@
+import {Controller, Get} from "@nestjs/common";
+
+
+@Controller('/tracks')
+export class TrackController {
+    create() {
+
+    }
+
+    @Get()
+    getAll() {
+        return 'work'
+    }
+
+    getOne() {
+
+    }
+
+    delete() {
+
+    }
+}
