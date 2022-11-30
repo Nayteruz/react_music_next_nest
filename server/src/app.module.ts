2743383019ab6e10937090d74ca08d0f1c8c51ff
Aleksandr Pankov <nayteruz@gmail.com>
2022-11-30 10:14:19 +0700

    Add env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,21 @@
-import {Module} from "@nestjs/common";
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 import {TrackModule} from "./track/track.module";
 import {MongooseModule} from "@nestjs/mongoose";
 import {FileModule} from "./file/file.module";
 import {ServeStaticModule} from "@nestjs/serve-static";
 import * as path from 'path'
 
+
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
-        MongooseModule.forRoot('mongodb+srv://nayteruz:19sunlight5s@cluster0.bxbc13e.mongodb.net/?retryWrites=true&w=majority'),
+        MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`),
         TrackModule,
         FileModule
 
     ]
 })
-export class AppModule {}
+export class AppModule {
+}
